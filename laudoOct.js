bb995cf722bba604c4b4953f852a95c06ccad739
eyeskafka <91967598+eyeskafka@users.noreply.github.com>
2022-02-16 14:06:06 -0300

    Add files via upload

---
+++ laudoOct.js
@@ -0,0 +1,12 @@
+function getall() {
+     var getall = document.getElementsByTagName('p');
+
+}
+function div1 () {
+     var div1 = document.getElementById('div1');
+      
+}
+function div2() {
+     var div2 = document.getElementById('div2');
+      
+} 
